60e996068fbb1008396290468b802c10babb4c60
Tetiana Fioshkina <fioshkina.tany@gmail.com>
2019-05-10 16:51:34 +0300

    KAFKA-324 Utils for authorization filter was created.

---
+++ ksql-rest-app/src/main/java/io/confluent/ksql/rest/client/exception/AuthorizationException.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2018 Confluent Inc.
+ *
+ * Licensed under the Confluent Community License; you may not use this file
+ * except in compliance with the License.  You may obtain a copy of the License at
+ *
+ * http://www.confluent.io/confluent-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package io.confluent.ksql.rest.client.exception;
+
+import io.confluent.rest.exceptions.RestException;
+
+import javax.ws.rs.core.Response;
+
+public class AuthorizationException extends RestException {
+
+  public static final int DEFAULT_ERROR_CODE = Response.Status.UNAUTHORIZED.getStatusCode();
+
+  public AuthorizationException(String message) {
+    this(message, null);
+  }
+
+  public AuthorizationException(String message, Throwable cause) {
+    super(message, DEFAULT_ERROR_CODE, DEFAULT_ERROR_CODE, cause);
+  }
+}
---
+++ ksql-rest-app/src/main/java/io/confluent/ksql/rest/filter/util/ByteProducerPool.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2018 Confluent Inc.
+ *
+ * Licensed under the Confluent Community License; you may not use this file
+ * except in compliance with the License.  You may obtain a copy of the License at
+ *
+ * http://www.confluent.io/confluent-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package io.confluent.ksql.rest.filter.util;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Future;
+
+import io.confluent.rest.impersonation.Errors;
+import org.apache.hadoop.security.UserGroupInformation;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+public class ByteProducerPool {
+
+  private Map<UserGroupInformation, KafkaProducer<byte[], byte[]>> producers =
+          new ConcurrentHashMap<>();
+
+  private Properties properties;
+
+  public ByteProducerPool(final Properties properties) {
+    this.properties = properties;
+  }
+
+  public Future<RecordMetadata> send(final ProducerRecord<byte[], byte[]> record) {
+    try {
+      final UserGroupInformation user = UserGroupInformation.getCurrentUser();
+
+      final KafkaProducer<byte[], byte[]> producer
+              = producers.computeIfAbsent(user, k -> new KafkaProducer<>(properties));
+
+      return producer.send(record);
+    } catch (IOException e) {
+      throw Errors.serverLoginException(e);
+    }
+  }
+
+  public void close() {
+    for (KafkaProducer producer : producers.values()) {
+      producer.close();
+    }
+  }
+}
